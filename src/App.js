907521b78ede84889892d2dba09e2a1c6b1ccfd4
tharkne <tharkne@umich.edu>
2021-03-26 06:09:37 -0400

    Reset pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header'
-import Homepage from './Homepage'
+import Homepage from './pages/Homepage'
+import { pageEnum } from './siteEnums'
+
+import { makeStyles } from '@material-ui/core'
+
+function pageSwitcher(page, setPage){
+  switch(page){
+    case pageEnum.HOME:
+      return (<Homepage setPage={setPage} />)
+    case pageEnum.PROGRAMMING:
+      return (<Programming setPage={setPage} />)
+    case pageEnum.ART:
+      return (<Art setPage={setPage} />)
+    case pageEnum.ABOUT:
+      return (<About setPage={setPage} />)
+    case pageEnum.RESUME:
+      return (<Resume setPage={setPage} />)
+  }
+}
 
 function App() {
+  const [page, setPage] = useState(0); 
+
   return (
     <div className="App">
       <Header>
-        <Homepage />
+        { pageSwitcher(page, setPage) }
       </Header>
     </div>
   );
